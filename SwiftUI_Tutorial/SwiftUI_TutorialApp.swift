e2c437af4a2a80ccf4258a236d9336d86f52524b
racoon97 <cherished322@gmail.com>
2022-12-12 19:57:56 +0900

    Initial Commit

---
+++ SwiftUI_Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI_Tutorial
+//
+//  Created by 강태윤 on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
