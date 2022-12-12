1c545f7bea941508817d2b2f522ce86f32bff914
racoon97 <cherished322@gmail.com>
2022-12-12 20:10:56 +0900

    Building Lists And Navigation Setting

---
+++ SwiftUI_Tutorial/Building Lists And Navigation/CircleImage.swift
@@ -0,0 +1,37 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorial
+//
+//  Created by 강태윤 on 2022/12/12.
+//
+
+/*
+ 
+ .clipShape(Circle()) - Image 모양 변경
+ 
+ .overay {
+    Circle().stroke(.gray, lineWidth: 4)
+ } - 회색 선을 가진 원을 만든 뒤 오버레이로 추가하여 Image 에 테두리 지정
+ 
+ .shadow(radius: 7) - 7 포인트 반경의 그림자 추가
+ 
+ */
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
